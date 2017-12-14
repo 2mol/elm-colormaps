38984748ce2b1156618298a7feacdf9e4659742c
talhoffer <talhoffer@users.noreply.github.com>
2017-12-14 13:46:18 +0100

    initial version

---
+++ src/Color/Colormaps.elm
@@ -0,0 +1,107 @@
+module Color.Colormaps
+    exposing
+        ( magma
+        , inferno
+        , plasma
+        , viridis
+        )
+
+{-|
+
+
+# Magma
+
+@docs magma
+
+
+# Inferno
+
+@docs inferno
+
+
+# Plasma
+
+@docs plasma
+
+
+# Viridis
+
+@docs viridis
+
+-}
+
+import Dict exposing (Dict)
+import Color exposing (Color, rgb)
+import Color.Colormaps.Matplotlib as MPL
+
+
+type alias Colormap =
+    Float -> Color
+
+
+{-| -}
+fromData : Dict Int ( Int, Int, Int ) -> Float -> Color
+fromData colorData t =
+    let
+        keys =
+            colorData |> Dict.keys
+
+        mminKey =
+            keys |> List.minimum
+
+        mmaxKey =
+            keys |> List.maximum
+
+        mminColor =
+            mminKey |> Maybe.andThen (\k -> Dict.get k colorData)
+
+        mmaxColor =
+            mmaxKey |> Maybe.andThen (\k -> Dict.get k colorData)
+
+        mtScaled =
+            mmaxKey
+                |> Maybe.map toFloat
+                |> Maybe.map2 (*) (Just t)
+                |> Maybe.map round
+
+        mcolorValue =
+            mtScaled |> Maybe.andThen (\k -> Dict.get k colorData)
+    in
+        case ( mcolorValue, mminColor, mmaxColor ) of
+            ( Just ( r, g, b ), _, _ ) ->
+                rgb r g b
+
+            ( Nothing, Just ( rMin, gMin, bMin ), Just ( rMax, gMax, bMax ) ) ->
+                if t < 0 then
+                    rgb rMin gMin bMin
+                else if t > 1 then
+                    rgb rMax gMax bMax
+                else
+                    rgb 1 0 1
+
+            _ ->
+                rgb 1 0 1
+
+
+{-| -}
+magma : Colormap
+magma =
+    fromData MPL.magmaData
+
+
+{-| -}
+inferno : Colormap
+inferno =
+    fromData MPL.infernoData
+
+
+{-| -}
+plasma : Colormap
+plasma =
+    fromData MPL.plasmaData
+
+
+{-| -}
+viridis : Colormap
+viridis =
+    fromData MPL.viridisData
